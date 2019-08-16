8cf99ffa204a8b5dbddc0bf943e72c758fe823f9
dragonbegin <juan.reyes.mba@gmail.com>
2019-08-15 21:17:16 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+
+const server = express();
+
+const userRouter = require("./users/userRouter");
+
+server.use(express.json());
+server.use(cors());
+server.use(logger);
+server.use("/api/users", userRouter);
+
+server.get("/", (req, res) => {
+    res.send(`<h2>Let's write some middleware!</h2>`);
+});
+
+//custom middleware
+
+function logger(req, res, next) {
+    console.log(
+        `Method: ${req.method}, url: ${
+            req.url
+        }, timestamp: ${new Date().toISOString()}`
+    );
+    next();
+}
+
+module.exports = server;
